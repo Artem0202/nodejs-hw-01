bdfa4f37d8a11e381b48f0ff60936b21b53c14e3
Artem0202 <artyomkov.19@gmail.com>
2024-06-09 19:15:50 +0300

    Homework 1.0

--- src/scripts/thanos.js
+++ src/scripts/thanos.js
@@ -1,5 +1,25 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const thanos = async () => {};
+export const thanos = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf8');
+    let dbData = JSON.parse(data);
+    const savedContacts = dbData
+      .map((contcat) => {
+        if (Math.random() >= 0.5) {
+          return contcat;
+        }
+      })
+      .filter((contcat) => contcat !== undefined);
+    const contactsList = await fs.writeFile(
+      PATH_DB,
+      JSON.stringify(savedContacts),
+      'utf-8',
+    );
+  } catch (err) {
+    console.error('Error:', err);
+  }
+};
 
 await thanos();
